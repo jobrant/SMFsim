9f8b80f04134305cc5323d44dddf64eaf97cca24
Brant <jobrant@ufl.edu>
2026-05-28 09:16:54 -0400

    Fix import call for data.table.

--- R/SMFsim-package.R
+++ R/SMFsim-package.R
@@ -1,4 +1,4 @@
-#' @importFrom data.table
+#' @import data.table
 #' @importFrom ggplot2 ggplot aes geom_bar geom_line theme_minimal labs
 #' @importFrom SMFnorm normalize_methylation_data find_shared_sites load_data
 #' "_PACKAGE"
