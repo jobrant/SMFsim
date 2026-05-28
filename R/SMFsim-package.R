3aad1be30f13686e89c0ff99333fafb36ee95206
Brant <jobrant@ufl.edu>
2026-05-28 09:32:12 -0400

    Fix issue with import export of package level functions.

--- R/SMFsim-package.R
+++ R/SMFsim-package.R
@@ -1,5 +1,5 @@
 #' @import data.table
 #' @importFrom ggplot2 ggplot aes geom_bar geom_line theme_minimal labs
 #' @importFrom SMFnorm normalize_methylation_data find_shared_sites load_data
-#' "_PACKAGE"
+"_PACKAGE"
 
